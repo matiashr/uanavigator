3f649e944a7a8e4ce4efe949384672448594180a
Your Name <you@example.com>
2020-11-15 00:56:48 +0100

    Well, what can I say, cut n paste..

--- src/basewindow.cpp
+++ src/basewindow.cpp
@@ -3,12 +3,12 @@
 *                                                                      	*
 * Copyright 2020 Matias Henttunen					*
 *                                                                      	*
-* FINAL CUT is free software; you can redistribute it and/or modify    	*
+* UA Navigator is free software; you can redistribute it and/or modify    	*
 * it under the terms of the GNU Lesser General Public License as       	*
 * published by the Free Software Foundation; either version 3 of       	*
 * the License, or (at your option) any later version.                  	*
 *                                                                      	*
-* FINAL CUT is distributed in the hope that it will be useful, but     	*
+* UA Navigator is distributed in the hope that it will be useful, but     	*
 * WITHOUT ANY WARRANTY; without even the implied warranty of           	*
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        	*
 * GNU Lesser General Public License for more details.                  	*
